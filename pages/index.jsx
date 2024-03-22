caae193a8f483e3d5d997de8b27622dacbb23a0b
akhsanby <akhsanby@gmail.com>
2024-03-22 11:15:57 +0700

    hide cv button & change tag line

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // components
 import Layout from "../src/components/Layout";
-import { Button } from "react-bootstrap";
+// import { Button } from "react-bootstrap";
 
 // custom style
 import styles from "../styles/Home.module.scss";
@@ -15,16 +15,16 @@ export default function Home() {
           <span>Hello, I'am</span>
           <span>Akhsan Bayu Riantama</span>
           <span>i'm Frontend Engineer</span>
-          <span>I'm tech enthusiasm who like coding in FrontEnd Development, I have goals to build apps with beautiful UI/UX design, high-quality code and clean architectures</span>
+          <span>I'm tech enthusiasm who like coding in FrontEnd Development, I have experience & want reach career as Frontend Engineer to build apps with beautiful UI/UX design, high-quality code and clean architectures</span>
         </h1>
-        <div className={styles.btn}>
-          {/* <Link href="/contact">
+        {/* <div className={styles.btn}>
+          <Link href="/contact">
             <Button variant="info">Contact Me</Button>
-          </Link> */}
+          </Link>
           <a href="/cv/my_cv.pdf">
             <Button variant="info">Download CV</Button>
           </a>
-        </div>
+        </div> */}
       </div>
     </Layout>
   );
